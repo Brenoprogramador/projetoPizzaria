8db8ef76e317e846c1ea8d4e2aea74f909c1ff5a
Matheus Henrique <mh3047449@gmail.com>
2023-08-30 18:17:47 -0300

    validação da variavel option

--- Program.cs
+++ Program.cs
@@ -1,37 +1,44 @@
+public class Program {
+    public static void Main() {
 
+        // VARIAVEIS
+        var pizza = new Pizza();
+        int option = 0;
 
-Console.WriteLine("Bem-vindo ao Projeto de Pizzaria");
-Console.WriteLine("--------------------------------");
+        while (option == 0 || option < 1 || option > 2) {
+            Console.Clear();
+            Console.WriteLine("Bem-vindo ao Projeto de Pizzaria");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("ESCOLHA UMA OPÇÃO");
+            Console.WriteLine("1 - Adicionar uma pizza");
+            Console.WriteLine("2 - Listas Pizzas");
+            Console.WriteLine(" ");
+            Console.WriteLine("Digite sua opção: ");
+            option = int.Parse(Console.ReadLine());
+        }
 
-Console.WriteLine("ESCOLHA UMA OPÇÃO");
-Console.WriteLine("1 - Adicionar uma pizza");
-Console.WriteLine("2 - Listas Pizzas");
-Console.WriteLine(" ");
-Console.WriteLine("Digite sua opção: ");
+        if (option == "1"){
+            Console.WriteLine("Adicionar uma pizza! ");
+            Console.WriteLine("Digite o nome da pizza: ");
+            var nome = Console.ReadLine();
 
-var option = Console.ReadLine();
-var pizza = new Pizza();
+            Console.WriteLine("Digite os ingredientes da pizza separado por virgula: ");
+            var ingredientes = Console.ReadLine().ToArray();
 
+            Console.WriteLine("Digite o preço da pizza: ");
+            var preco = int.Parse(Console.ReadLine());
 
-if (option == "1"){
-    Console.WriteLine("Adicionar uma pizza! ");
-    Console.WriteLine("Digite o nome da pizza: ");
-    var nome = Console.ReadLine();
+            pizza.Nome = nome;
+            pizza.Ingredientes = ingredientes;
+            pizza.Preco = preco;
 
-    Console.WriteLine("Digite os ingredientes da pizza separado por virgula: ");
-    var ingredientes = Console.ReadLine().ToArray();
+            Console.WriteLine(pizza);
+        }
 
-    Console.WriteLine("Digite o preço da pizza: ");
-    var preco = int.Parse(Console.ReadLine());
+        
 
-    pizza.Nome = nome;
-    pizza.Ingredientes = ingredientes;
-    pizza.Preco = preco;
+    }
+} 
 
-    Console.WriteLine(pizza);
-};
-
-
-
-
+ 
 
--- models/Pizza.cs
+++ models/Pizza.cs
@@ -1,6 +1,13 @@
 ﻿public class Pizza {
     public string Nome {get; set;}
     public int Preco {get; set;}
-    public char[] Ingredientes {get; set;}
-};
+    public string[] Ingredientes {get; set;} 
+
+    public void pizza(string Nome, int Preco, string[] Ingredientes) {
+        this.Nome = Nome;
+        this.Preco = Preco;
+        this.Ingredientes = Ingredientes;
+    }
+}
+
 
